397a7870c04297eb406cfa4cc8236af9cfe5f1a2
Ece Aybike Ala <44816131+eceala@users.noreply.github.com>
2018-11-09 17:57:04 +0100

    Create en-kucuk-en-buyuk-deger-bulma.m
    5x5lik bir matris tanimla. En yuksek degeri barindiran satir numarası ve en kucuk degeri barindiran sutunun numarasini bulup topla.

---
+++ en-kucuk-en-buyuk-deger-bulma.m
@@ -0,0 +1,21 @@
+
+a = randi (10,5,5);
+
+max = a (1,1);
+min = a (1,1);
+satir = 1;
+sutun = 1;
+
+for i=1:5
+    for j=1:5
+        deger = a(i,j);
+
+        if deger > max %max degeri surekli guncellenecek
+           max = deger;
+           satir = i;
+        elseif deger < min
+            min = deger;
+            sutun = j;
+        end
+    end
+end
